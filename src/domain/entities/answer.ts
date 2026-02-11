61080f114ee8bdc95afe77803a98d51ccda59891
Giovani <giovani_marchezine@hotmail.com>
2026-02-10 22:48:30 -0300

    Iniciando entidades e iniciando tests

---
+++ src/domain/entities/answer.ts
@@ -0,0 +1,10 @@
+import { randomUUID } from "node:crypto";
+
+export class Answer {
+  public content: string;
+  public id: string;
+  constructor(content: string, id?: string) {
+    this.content = content;
+    this.id = id ?? randomUUID();
+  }
+}
---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question.js";
+
+test("create an answer", () => {
+  const answerQuestion = new AnswerQuestionUseCase();
+  const answer = answerQuestion.execute({
+    questionId: "1",
+    instructorId: "2",
+    content: "Nova resposta",
+  });
+
+  expect(answer.content).toEqual("Nova resposta");
+});
